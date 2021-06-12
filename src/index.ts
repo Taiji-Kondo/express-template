f3574f35e0cbb0e6f89e85291003dd2c7e541e02
Taiji-Kondo <k.taiji1108@gmail.com>
2021-06-12 17:32:55 +0900

    SETTING: MySQLのインストール

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express from 'express'
 const app = express()
 const port = 8000
 
-app.get('/', (req, res) => res.send(`Hello World! ${req}`))
+app.get('/', (req, res) => {
+  res.send(`Hello World! ${req}`)
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
