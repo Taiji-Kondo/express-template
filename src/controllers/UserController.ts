f88532b10ee8a8b2d80e64f0ebc258985b75d03e
Taiji Kondo <kondo@sailboat.co.jp>
2021-06-16 19:58:18 +0900

    CREATE: ORMにconnectionしてusersを返すメソッド作成

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,11 +1,20 @@
-import { Controller, Get, JsonController } from 'routing-controllers'
+import { Get, JsonController } from 'routing-controllers'
 import { Users } from '../models/Users'
+import { createConnection, getConnectionOptions } from 'typeorm'
 
-@JsonController()
-@Controller('/users')
+@JsonController('/users')
 export class UserController {
   @Get('/')
-  getAll() {
-    return Users
+  async getAll() {
+    // TODO
+    // connectionの設定を書いたクラスを継承して使い回す
+    const connectionOptions = await getConnectionOptions('default')
+    const connection = await createConnection(connectionOptions)
+    const user = new Users()
+    user.name = 'test'
+    user.age = 24
+    await connection.getRepository(Users).save(user)
+    const users = await connection.getRepository(Users).find()
+    return users
   }
 }
