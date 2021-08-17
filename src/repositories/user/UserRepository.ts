6e2e0a71ab9c71fb8176cc6f7b9dd988a593a57c
Taiji Kondo <kondo@sailboat.co.jp>
2021-08-17 19:13:59 +0900

    CREATE: エンドポイント作成

--- src/repositories/user/UserRepository.ts
+++ src/repositories/user/UserRepository.ts
@@ -20,8 +20,23 @@ export class UserRepository extends Repository<Users> {
     }
   }
 
+  async getUser(_id: number): Promise<ResponseType<UserType[]>> {
+    const connection = await this.createConnection()
+    try {
+      const user = await connection.getRepository(Users).findOne(_id)
+      return { data: user ? [user] : [] }
+    } catch (err) {
+      return { data: err }
+    } finally {
+      await connection.close()
+    }
+  }
+
   async createUser(_user: UserType): Promise<ResponseType<string>> {
     const connection = await this.createConnection()
+    // if create relational data
+    // const user = _user.user
+    // user.hobby = _user.hobby
     try {
       await connection.getRepository(Users).save(_user)
       return { data: 'success' }
@@ -31,6 +46,28 @@ export class UserRepository extends Repository<Users> {
       await connection.close()
     }
   }
+
+  // if need update
+  // async updateUser(_id: number, _user: UserType): Promise<ResponseType<string>> {
+  //   const connection = await this.createConnection()
+  //   const user = { id: _id, ..._user.user }
+  //   user.mountainDetail = _user.hobby
+  //
+  //   try {
+  //     const userHobby = await connection
+  //       .getRepository(Users)
+  //       .findOne(_id, { relations: ['userHobby'] })
+  //     if (userHobby) {
+  //       user.mountainDetail.id = userHobby?.id
+  //     }
+  //     await connection.getRepository(Users).save(user)
+  //     return { data: 'success' }
+  //   } catch (err) {
+  //     return { data: err }
+  //   } finally {
+  //     await connection.close()
+  //   }
+  // }
 
   async deleteUser(_id: number): Promise<ResponseType<string>> {
     const connection = await this.createConnection()
