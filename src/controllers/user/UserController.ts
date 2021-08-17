7f6a9cf506db4dad6b316726c5884536fd0ade83
Taiji Kondo <kondo@sailboat.co.jp>
2021-08-17 18:15:25 +0900

    REFACTOR: responseの値を変更

--- src/controllers/user/UserController.ts
+++ src/controllers/user/UserController.ts
@@ -1,25 +1,27 @@
 import { Body, Delete, Get, JsonController, Param, Post } from 'routing-controllers'
 import { UserRepository } from '../../repositories/user/UserRepository'
-import { Users } from '../../models/user/Users'
 import { UserType } from '../../types/user/Users'
+import { ResponseType } from '../../types/ResponseType'
 
 @JsonController('/users')
 export class UserController {
   private readonly userRepository = new UserRepository()
 
   @Get('/')
-  async getAll(): Promise<Users[]> {
+  async getAll(): Promise<ResponseType<UserType[]>> {
     const response = await this.userRepository.getUsers()
     return response
   }
 
   @Post('/create')
-  async create(@Body() user: UserType): Promise<void> {
-    await this.userRepository.createUser(user)
+  async create(@Body() user: UserType): Promise<ResponseType<string>> {
+    const response = await this.userRepository.createUser(user)
+    return response
   }
 
   @Delete('/delete/:id')
-  async delete(@Param('id') id: number): Promise<void> {
-    await this.userRepository.deleteUser(id)
+  async delete(@Param('id') id: number): Promise<ResponseType<string>> {
+    const response = await this.userRepository.deleteUser(id)
+    return response
   }
 }
--- src/repositories/user/UserRepository.ts
+++ src/repositories/user/UserRepository.ts
@@ -1,34 +1,52 @@
 import { Users } from '../../models/user/Users'
 import { Repository } from '../Repository'
 import { UserType } from '../../types/user/Users'
+import { ResponseType } from '../../types/ResponseType'
 
 export class UserRepository extends Repository<Users> {
   constructor() {
     super(new Users())
   }
 
-  async getUsers(): Promise<Users[]> {
+  async getUsers(): Promise<ResponseType<UserType[]>> {
     const connection = await this.createConnection()
-    const users = await connection.getRepository(Users).find()
-    await connection.close()
-    return users
+    try {
+      const users = await connection.getRepository(Users).find()
+      return { data: users }
+    } catch (err) {
+      return { data: err }
+    } finally {
+      await connection.close()
+    }
   }
 
-  async createUser(_user: UserType): Promise<void> {
+  async createUser(_user: UserType): Promise<ResponseType<string>> {
     const connection = await this.createConnection()
-    const user = _user
-    await connection.getRepository(Users).save(user)
-    await connection.close()
+    try {
+      const user = _user
+      await connection.getRepository(Users).save(user)
+      return { data: 'success' }
+    } catch (err) {
+      return { data: err }
+    } finally {
+      await connection.close()
+    }
   }
 
-  async deleteUser(_id: number): Promise<void> {
+  async deleteUser(_id: number): Promise<ResponseType<string>> {
     const connection = await this.createConnection()
-    await connection
-      .createQueryBuilder()
-      .delete()
-      .from(Users)
-      .where('id = :id', { id: _id })
-      .execute()
-    await connection.close()
+    try {
+      await connection
+        .createQueryBuilder()
+        .delete()
+        .from(Users)
+        .where('id = :id', { id: _id })
+        .execute()
+      return { data: 'success' }
+    } catch (err) {
+      return { data: err }
+    } finally {
+      await connection.close()
+    }
   }
 }
